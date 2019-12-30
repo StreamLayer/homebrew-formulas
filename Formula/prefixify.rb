ba9f768d8d199df2b621b5ffa68999fe796b2d4b
AVVS <v@makeomatic.ca>
2019-12-30 14:52:52 -0800

    Updated swift-prefixify formula

--- Formula/prefixify.rb
+++ Formula/prefixify.rb
@@ -2,9 +2,9 @@ class Prefixify < Formula
   desc "Prefixes swift public & open identifier"
   homepage "https://github.com/streamlayer/prefixify"
   url "https://github.com/streamlayer/prefixify.git",
-      :tag      => "0.0.6+streamlayer",
-      :version  => "0.0.6",
-      :revision => "6047f9460f367ad66d3650e77cdc5a74991c0b4d",
+      :tag      => "0.0.7+streamlayer",
+      :version  => "0.0.7",
+      :revision => "52eb80fb907d3382d6d1b70a14939ad262041d1a",
       :shallow  => false
   head "https://github.com/streamlayer/prefixify.git", :shallow => false
 
@@ -13,7 +13,7 @@ class Prefixify < Formula
   bottle do
     root_url "https://dl.bintray.com/streamlayer/bottles-formulas"
     cellar :any_skip_relocation
-    sha256 "80dfba4ad684b981ea9ea97c5940648fb8d8d89362f23351c241fbb2cc3eb629" => :catalina
+    sha256 "da7d43c62b72e28b9a09b0b29f68bca665e52db8e42906e7797bcc0c2508bbe9" => :catalina
   end
 
   def install
